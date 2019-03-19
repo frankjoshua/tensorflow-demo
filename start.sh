750fe02ba36e586656eaea870fcc2841f39b660c
josh <josh@tesseractmobile.com>
2019-03-18 23:27:30 -0500

    Added tensoboard

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8888:8888 -v $PWD:/notebooks tensorflow/tensorflow # Start a Jupyter notebook server 
+docker run -it -p 8888:8888 -p 6006:6006 -v $PWD:/notebooks tensorflow/tensorflow # Start a Jupyter notebook server 

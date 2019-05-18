20eceab7cd6eba22cd04390c1bf2e6a92983eca1
josh <josh@tesseractmobile.com>
2019-05-17 20:43:48 -0500

    Added more images

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8888:8888 -p 6006:6006 -v $PWD:/notebooks tensorflow/tensorflow # Start a Jupyter notebook server 
+docker run -it -p 8888:8888 -p 6006:6006 -v $PWD/notebooks:/tf tensorflow/tensorflow:latest-py3-jupyter # Start a Jupyter notebook server 
